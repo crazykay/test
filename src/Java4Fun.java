bc42fbdb4f2dc260ceab2b40940b130e5c73e029
crazykay <kai64286513@gmail.com>
2013-11-17 00:15:33 +0800

    新文件：    src/Java4Fun.java 	新文件：    src/threadtest/ThreadTest.java 	删除：      src/com/crazykay/test/Java4Fun.java

---
+++ src/Java4Fun.java
@@ -0,0 +1,14 @@
+public class Java4Fun {
+
+	/**
+	 * @param args
+	 * @author crazykay
+	 * @throws ClassNotFoundException
+	 * @category b>JavaFX</b>
+	 */
+	public static void main(String[] args) throws ClassNotFoundException {
+		int x = 2,y =6;
+		System.out.println(x^y);
+	}
+
+}
